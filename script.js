61ab40c5fe21952c52d35a492200431d057cc6f4
Aayush Chouhan <aayushchouhan24@gmail.com>
2023-10-30 23:33:50 +0530

    new feature

--- script.js
+++ script.js
@@ -1,27 +1,112 @@
-const textInput = document.querySelector(".text-input")
-const imageInput = document.querySelector(".file input")
-const img = document.querySelector(".file img")
+const imageInput = document.querySelector(".image input")
+const audioInput = document.querySelector(".audio input")
 
-document.getElementById('encrypt-button').addEventListener('click', () => {
-  if (img.src) {
-    document.querySelector(".head img").style.display = 'block'
-    document.querySelector(".head p").textContent = ''
-    Stego.encode_LSB_Stenography(img.src, textInput.value, img => document.querySelector(".head img").src = img)
+const imageDisplay = document.querySelector(".imagesteg .head img")
+const audioDisplay = document.querySelector(".audiosteg .head img")
+
+const imageSecret = document.querySelector(".imagesteg .head p")
+const audioSecret = document.querySelector(".audiosteg .head p")
+
+
+
+let img = null
+let audio = null
+
+
+document.querySelector('.encrypt-image').addEventListener('click', () => {
+  if (!img) alert('Please select an image file.', 'warning')
+  else {
+    try {
+      Stego.encodeImage_LSB(img.src, document.querySelector(".imagesteg .text-input").value, img => {
+        imageDisplay.src = img
+        imageDisplay.style.opacity = '1'
+        imageSecret.style.opacity = '0'
+        imageSecret.textContent = ''
+        alert("Your secrete message encoded successfully", 'success')
+      })
+    } catch (error) {
+      alert("Encoding unsuccessfully", 'danger')
+      console.log(error)
+    }
   }
 })
 
-document.getElementById('decrypt-button').addEventListener('click', () => {
-  if (img.src) {
-    document.querySelector(".head img").style.display = 'none'
-    Stego.decode_LSB_Stenography(img.src, txt => document.querySelector(".head p").textContent = txt)
-  }
+document.querySelector('.decrypt-image').addEventListener('click', () => {
+  if (!img) alert('Please select an image file.', 'warning')
+  else
+    try {
+      Stego.decodeImage_LSB(img.src, x => {
+        imageSecret.textContent = x
+        imageSecret.style.opacity = '1'
+        imageDisplay.style.opacity = '0'
+        alert("Your secret message decoded successfully", 'success')
+      })
+    }
+    catch (error) {
+      alert("Decoding unsuccessfully", 'danger')
+      console.log(error)
+    }
 })
 
 imageInput.addEventListener('change', () => {
+  img = document.querySelector(".image img")
   img.src = URL.createObjectURL(imageInput.files[0])
   img.style.display = 'block  '
-  document.querySelector('.file svg').style.display = 'none'
+  document.querySelector('.imagesteg').classList.add('activeImage')
 })
+
+document.querySelector('.encrypt-audio').addEventListener('click', () => {
+  if (!audio) alert('Please select an audio file.', 'warning')
+  else
+    try {
+      Stego.encodeAudio_LSB(audio, document.querySelector(".audiosteg .text-input").value, x => {
+        audioElement(x)
+        audioDisplay.style.opacity = '1'
+        audioSecret.style.opacity = '0'
+        audioSecret.textContent = ''
+        alert("Your secrete message encoded successfully", 'success')
+      })
+    } catch (error) {
+      alert("Encoding unsuccessfully", 'danger')
+      console.log(error)
+    }
+})
+
+document.querySelector('.decrypt-audio').addEventListener('click', () => {
+  if (!audio) alert('Please select an audio file.', 'warning')
+  else try {
+    Stego.decodeAudio_LSB(audio, x => {
+      audioDisplay.style.opacity = '0'
+      audioSecret.style.opacity = '1'
+      audioSecret.textContent = x
+    })
+  }
+  catch (error) {
+    alert("Decoding unsuccessfully", 'danger')
+    console.log(error)
+  }
+})
+
+audioInput.addEventListener('change', () => {
+  const audioFile = audioInput.files[0]
+  const reader = new FileReader()
+  reader.onload = e => audio = e.target.result
+  reader.readAsArrayBuffer(audioFile)
+  document.querySelector('.audiosteg').classList.add('encodeAudio')
+  audioDisplay.style.opacity = '1'
+  audioSecret.style.opacity = '0'
+  audioSecret.textContent = ''
+})
+
+dragElement(document.querySelector('.imagesteg'), document.querySelector('.imagesteg h2'))
+dragElement(document.querySelector('.audiosteg'), document.querySelector('.audiosteg h2'))
+
+
+function audioElement(src) {
+  document.querySelector('.audiosteg').classList.add('activeAudio')
+  document.querySelector('.audiosteg').classList.remove('encodeAudio')
+  document.querySelector("audio").src = src
+}
 
 function dragElement(element, trigger) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
@@ -54,4 +139,48 @@ function dragElement(element, trigger) {
   }
 }
 
-dragElement(document.querySelector('.card'), document.querySelector('.head'))
+function alert(message, type = 'info', duration = 4000) {
+  const alert = document.createElement('div')
+  alert.classList.add('alert', `alert-${type}`)
+
+  const icon = document.createElement('span')
+  icon.classList.add('icon')
+  const iconClass = {
+    'default': 'fa-superpowers',
+    'success': 'fa-check',
+    'warning': 'fa-exclamation-triangle',
+    'danger': 'fa-close',
+    'info': 'fa-info'
+  }[type]
+  icon.innerHTML = `<i class="fa ${iconClass}"></i>`
+
+  const text = document.createElement('div')
+  text.classList.add('text')
+  text.innerHTML = `
+      <strong>${type.charAt(0).toUpperCase() + type.slice(1)}</strong>
+      <p>${message}</p>
+  `
+
+  const closeButton = document.createElement('button')
+  closeButton.classList.add('close')
+  closeButton.innerHTML = '<i class="fa fa-close"></i>'
+
+  alert.appendChild(icon)
+  alert.appendChild(text)
+  alert.appendChild(closeButton)
+
+  closeButton.addEventListener('click', close)
+
+  setTimeout(close, duration)
+
+  function close() {
+    alert.classList.add('close')
+    alert.addEventListener('transitionend', function (event) {
+      if (event.propertyName === 'transform') {
+        alert.remove()
+      }
+    })
+  }
+
+  document.body.appendChild(alert)
+}
